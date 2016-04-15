a002127db8351881c37de619e6ec9fc38080d426
BrucePaterson <bruce.paterson@paterson2.com>
2016-04-14 23:52:51 -0400

    Added files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -45,7 +45,7 @@
     
     ##Create index1 using a regular expression where names start with 't' and have either 'mean' or 'std' in
     ##name.
-    index1 <- grep("^t.*(mean|std)",tf,value = FALSE)
+    index1 <- grep("mean|std",tf,value = FALSE)
     headers <- tf[index1]  
     
     ##Extract the X_data columns from the training and test data files (vartrain and vartest above)
@@ -53,27 +53,29 @@
     variables <- variables[index1]
     names(variables) <- headers
     
-    ##Tidy data set Step 4. created
+    ##Tidy data set Step 4. created:
     tidydat1 <- cbind(subact,variables)  ##tidy data set 1 combining subact and variables
     library(reshape2)                    ##need reshape2 package to melt tidydat1
     id <-1:length(tidydat1[,1])          ##create a unique id for each row of tidydat1 (for melt)
     tidydat1 <-cbind(id,tidydat1)        ##add unique id column to front of tidydat1 before melt
+    
     ##Melt tidydat1 to put "variables" in rows of data from columns currently usig first 3 cols as 
     ##id.vars
     tidydat1 <- melt(tidydat1,id.vars = c(1,2,3))
+    
     ##Split data into tables/data.frames (40) where each has only one observation/value per row 
     tidydat1 <- split(tidydat1,tidydat1$variable)
     
-    ##Create the final tidy data set for Step 5 - by extracting names with "mean" in them from tidydat1
-    ##there are 20 data.frames extracted
     
-    tidydat2 <- tidydat1[grep("mean",names(tidydat1),value = FALSE)]
+    ##Create the final tidy data set for Step 5: 
     
     ##You can extract the data.frames from the list by using "ldply" function from "plyr" and then
     ##recombine them to create one data.frame for analysis.
-    tiddtrecreate <- tidydat2[1:length(names(tidydat2))] 
+    tiddtrecreate <- tidydat1[1:length(names(tidydat1))] 
     library(dplyr)
     tiddtrecreate <- ldply(tiddtrecreate, data.frame) %>% select(2:6)
     
-    ##write.table (tidydat2,file = "tidyout.txt",row.names = FALSE) output the tidy data as per Step 5.
+    tidydat2 <- ddply(tiddtrecreate,c("subjectid","activity","variable"),summarise,mean = mean(value))
+    
+    write.table (tidydat2,file = "tidyout.txt",row.names = FALSE) ##output the tidy data as per Step 5.
     ##instructions as a text file.
